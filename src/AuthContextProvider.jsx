578e5f1338070fe562a4d18a7e0664167db54fa2
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-06-23 11:41:53 -0400

    Create global authentication context for app

---
+++ src/AuthContextProvider.jsx
@@ -0,0 +1,46 @@
+import { jwtDecode } from "jwt-decode";
+import { createContext, useReducer } from "react";
+
+export const AuthContext = createContext({
+   user: null,
+});
+
+const userReducer = (user, action) => {
+   switch (action.type) {
+      case "LOGIN":
+         return {
+            name: action.name,
+            id: action.id,
+            token: action.token,
+            author: action.author,
+         };
+      case "LOGOUT":
+         return null;
+      default:
+         return user;
+   }
+};
+
+export const AuthContextProvider = ({ children }) => {
+   // initialize user state if user exists in local storage
+   const initializeUser = () => {
+      const storedUser = JSON.parse(localStorage.getItem("user"));
+      let user = null;
+      if (storedUser) {
+         const { id, author } = jwtDecode(storedUser.token);
+         const name = storedUser.name;
+         user = {
+            token: storedUser.token,
+            id,
+            name,
+            author,
+         };
+      }
+
+      return user;
+   };
+
+   const [user, userDispatch] = useReducer(userReducer, null, initializeUser);
+
+   return <AuthContext.Provider value={{ user, userDispatch }}>{children}</AuthContext.Provider>;
+};
