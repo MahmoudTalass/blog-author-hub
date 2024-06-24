f30dfae405dcdae3daba62740f7721e2871370c0
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-06-24 18:51:22 -0400

    Add simple style to footer

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,6 +1,6 @@
 export function Footer() {
    return (
-      <footer className="flex items-center justify-center h-12 mt-auto bg-slate-600">
+      <footer className="flex items-center justify-center h-12 mt-auto bg-color3">
          <p>Copyright &copy; 2024 Mahmoud T</p>
       </footer>
    );
