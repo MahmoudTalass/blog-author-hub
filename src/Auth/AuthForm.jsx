5499e51aff2f0504d6a8abd153671a097b292428
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-06-24 19:32:18 -0400

    Include onSubmit function prop for submitting form

--- src/Auth/AuthForm.jsx
+++ src/Auth/AuthForm.jsx
@@ -1,9 +1,12 @@
 import PropTypes from "prop-types";
 
-export function AuthForm({ children }) {
+export function AuthForm({ children, onSubmit }) {
    return (
       <div className="w-full flex justify-center">
-         <form className="w-[400px] min-w-[300px] bg-color1 flex flex-col items-center p-4 gap-6 mt-10 rounded-md">
+         <form
+            className="w-[400px] min-w-[300px] bg-color1 flex flex-col items-center p-4 gap-6 mt-10 rounded-md"
+            onSubmit={onSubmit}
+         >
             {" "}
             {children}
          </form>
@@ -13,4 +16,5 @@ export function AuthForm({ children }) {
 
 AuthForm.propTypes = {
    children: PropTypes.any,
+   onSubmit: PropTypes.func,
 };
