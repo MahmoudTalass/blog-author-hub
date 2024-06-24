bb1c565783b9c658b0caa0022b6283e2f0f60951
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-06-24 19:33:25 -0400

    Expect value state and value setter function props

--- src/Auth/AuthInputField.jsx
+++ src/Auth/AuthInputField.jsx
@@ -1,6 +1,6 @@
 import PropTypes from "prop-types";
 
-export function AuthInputField({ name, type, label, id }) {
+export function AuthInputField({ name, type, label, id, value, onChange }) {
    return (
       <div className="flex flex-col items-center w-full gap-2">
          <label htmlFor={id} className="text-xl">
@@ -13,6 +13,8 @@ export function AuthInputField({ name, type, label, id }) {
             required
             className="bg-color3 rounded w-5/6 h-7 px-2"
             autoComplete="true"
+            onChange={onChange}
+            value={value}
          />
       </div>
    );
@@ -23,4 +25,6 @@ AuthInputField.propTypes = {
    type: PropTypes.string,
    label: PropTypes.string,
    id: PropTypes.string,
+   value: PropTypes.string,
+   onChange: PropTypes.func,
 };
