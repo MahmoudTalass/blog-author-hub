bd4dc49d066b190f20bfcfff244af2d338e5b252
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-06-24 10:47:55 -0400

    Create simple footer

---
+++ src/Footer.jsx
@@ -0,0 +1,7 @@
+export function Footer() {
+   return (
+      <footer className="flex items-center ">
+         <p className="text-center">Copyright &copy; 2024 Mahmoud T</p>
+      </footer>
+   );
+}
