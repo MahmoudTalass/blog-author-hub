4872751a24ff6dfbbab50265f8b3ea5876a6b529
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-06-24 18:45:49 -0400

    Set page colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
    content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
    theme: {
-      extend: {},
+      extend: {
+         colors: {
+            color1: "#1C2833",
+            color2: "#2C3E50",
+            color3: "#34495E",
+            fontcolor: "#ECF0F1",
+         },
+      },
    },
    plugins: [],
 };
