1740f2ecc85cc217918b3d696868a0280fbfe6d3
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-06-24 09:49:44 -0400

    Update auth related imports

--- src/Router.jsx
+++ src/Router.jsx
@@ -1,8 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 import PageLayout from "./PageLayout";
 import Home from "./Home";
-import Signup from "./Signup";
-import Login from "./Login";
+import Signup from "./Auth/Signup";
+import Login from "./Auth/Login";
 
 const router = createBrowserRouter([
    {
