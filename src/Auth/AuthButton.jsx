9031f6957b539decf8ea582367ea56da6385ca40
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-06-24 19:22:00 -0400

    Make the button type submit

--- src/Auth/AuthButton.jsx
+++ src/Auth/AuthButton.jsx
@@ -1,7 +1,11 @@
 import PropTypes from "prop-types";
 
 export function AuthButton({ children }) {
-   return <button className="border px-5 py-2 rounded-md">{children}</button>;
+   return (
+      <button className="border px-5 py-2 rounded-md" type="submit">
+         {children}
+      </button>
+   );
 }
 AuthButton.propTypes = {
    children: PropTypes.string,
