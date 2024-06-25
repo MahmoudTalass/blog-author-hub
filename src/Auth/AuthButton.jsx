7874942a78c1f7ff4872288ff3e2ba5ee686b24f
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-06-24 21:16:59 -0400

    Create prop that determines if button should be disabled
    This will be used to avoid having the user submit consecutive requests
    while one request is still being processed.

--- src/Auth/AuthButton.jsx
+++ src/Auth/AuthButton.jsx
@@ -1,12 +1,13 @@
 import PropTypes from "prop-types";
 
-export function AuthButton({ children }) {
+export function AuthButton({ children, disabled }) {
    return (
-      <button className="border px-5 py-2 rounded-md" type="submit">
+      <button className="border px-5 py-2 rounded-md" type="submit" disabled={disabled}>
          {children}
       </button>
    );
 }
 AuthButton.propTypes = {
    children: PropTypes.string,
+   disabled: PropTypes.bool,
 };
