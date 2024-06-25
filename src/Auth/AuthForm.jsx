1380ba43f7ed77a6e688cea9856739c684b1af7d
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-06-24 21:18:34 -0400

    Fix naming of the handle auth function

--- src/Auth/AuthForm.jsx
+++ src/Auth/AuthForm.jsx
@@ -1,11 +1,11 @@
 import PropTypes from "prop-types";
 
-export function AuthForm({ children, onSubmit }) {
+export function AuthForm({ children, handleAuth }) {
    return (
       <div className="w-full flex justify-center">
          <form
             className="w-[400px] min-w-[300px] bg-color1 flex flex-col items-center p-4 gap-6 mt-10 rounded-md"
-            onSubmit={onSubmit}
+            onSubmit={handleAuth}
          >
             {" "}
             {children}
@@ -16,5 +16,5 @@ export function AuthForm({ children, onSubmit }) {
 
 AuthForm.propTypes = {
    children: PropTypes.any,
-   onSubmit: PropTypes.func,
+   handleAuth: PropTypes.func,
 };
