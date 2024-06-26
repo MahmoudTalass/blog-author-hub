390dc42bb093c06d6180d085f10e5c555e69054f
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-06-25 21:13:00 -0400

    Create post preview card component

---
+++ src/PostCard.jsx
@@ -0,0 +1,19 @@
+import moment from "moment";
+import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+
+export function PostCard({ post }) {
+   return (
+      <Link to={`/post/${post._id}`}>
+         <article className="flex flex-col gap-5 w-[500px] w-min-[300px] p-5 bg-color1 rounded-lg">
+            <h3 className="text-3xl ">{post.title}</h3>
+            <p className="">By: {post.author.name}</p>
+            <p className="text-right">{moment(post.publishDate).format("LLL")}</p>
+         </article>
+      </Link>
+   );
+}
+
+PostCard.propTypes = {
+   post: PropTypes.object,
+};
