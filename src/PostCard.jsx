0e42bd683305aa8ecc5bcc41f387dd10e5e0ef53
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-06-26 09:55:06 -0400

    Display publication status

--- src/PostCard.jsx
+++ src/PostCard.jsx
@@ -6,9 +6,20 @@ export function PostCard({ post }) {
    return (
       <Link to={`/post/${post._id}`}>
          <article className="flex flex-col gap-5 w-[500px] w-min-[300px] p-5 bg-color1 rounded-lg">
-            <h3 className="text-3xl ">{post.title}</h3>
+            <div className="flex justify-between">
+               <h3 className="text-3xl ">{post.title}</h3>
+               <div
+                  className={`bg-${post.isPublished ? "green-700" : "red-500"} rounded-md
+                   flex justify-center items-center px-2`}
+               >
+                  {post.isPublished ? "Published" : "Unpublished"}
+               </div>
+            </div>
             <p className="">By: {post.author.name}</p>
-            <p className="text-right">{moment(post.publishDate).format("LLL")}</p>
+            {post.isPublished && (
+               <p className="">Publish date: {moment(post.publishDate).format("ll")}</p>
+            )}
+            <p className="">Creation date: {moment(post.createdAt).format("ll")}</p>
          </article>
       </Link>
    );
