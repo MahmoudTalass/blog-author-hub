579a71a890b53d2940048e758d39c4abc8b9ecf2
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-06-27 19:39:24 -0400

    Create custom hook for adding/updating data on the api

---
+++ src/posts/usePostData.jsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import { useAuthContext } from "../auth/useAuthContext";
+import { useLogout } from "../auth/useLogout";
+
+export function usePostData(url, fetchMethod) {
+   const [error, setError] = useState(null);
+   const [isLoading, setIsLoading] = useState(false);
+   const [data, setData] = useState(false);
+   const { user } = useAuthContext();
+   const logout = useLogout();
+
+   const postData = async (body) => {
+      setIsLoading(true);
+      try {
+         const response = await fetch(url, {
+            method: fetchMethod,
+            headers: {
+               Authorization: `Bearer ${user.token}`,
+               "content-type": "application/json",
+            },
+            body: JSON.stringify(body),
+         });
+
+         if (response.status === 401) {
+            logout();
+            throw new Error("Authorization required");
+         }
+
+         const json = await response.json();
+
+         if (response.status === 400) {
+            return setError(json.error);
+         }
+
+         if (!response.ok) {
+            throw new Error(json.error.message);
+         }
+         setData(json);
+         setError(null);
+      } catch (err) {
+         setError(err);
+      } finally {
+         setIsLoading(false);
+      }
+   };
+
+   return { data, postData, error, isLoading };
+}
