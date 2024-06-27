47d845bac3b5b69c7c29a7a83a45153a5ed2b22a
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-06-27 10:38:15 -0400

    Display post previews and enable adding new posts

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,3 +1,67 @@
+import { useState, useMemo } from "react";
+import { useAuthContext } from "./auth/useAuthContext";
+import { Navigate, Link } from "react-router-dom";
+import { PostCard } from "./posts/PostCard";
+import { useFetch } from "./useFetch";
+
 export function Home() {
-   return <p>Hello</p>;
+   const { user } = useAuthContext();
+   const [selectedFilter, setSelectedFilter] = useState("all");
+   const { data, actions, error, isLoading } = useFetch("http://localhost:3000/api/users/me/posts");
+
+   const handleFilterChange = (e) => setSelectedFilter(e.target.value);
+
+   const filteredPosts = useMemo(() => {
+      if (selectedFilter === "all") {
+         return data;
+      } else if (selectedFilter === "published") {
+         return data.filter((post) => post.isPublished === true);
+      } else {
+         return data.filter((post) => post.isPublished === false);
+      }
+   }, [data, selectedFilter]);
+
+   if (!user) return <Navigate to="/login" />;
+
+   if (isLoading) return <p>Loading...</p>;
+
+   if (error) {
+      return <p>{error.message}</p>;
+   }
+
+   return (
+      <div className="m-4 flex justify-center">
+         <div className="flex flex-col max-w-[1200px]">
+            <div className="flex justify-between">
+               <Link to="/create-post">
+                  <button className="px-4 py-2 bg-color1 rounded-md">Create new post</button>
+               </Link>
+               <select
+                  name="publicationStatus"
+                  id="publicationStatus"
+                  className="bg-color1 ml-auto p-2 rounded-md"
+                  value={selectedFilter}
+                  onChange={handleFilterChange}
+               >
+                  <option value="all">All</option>
+                  <option value="published">published</option>
+                  <option value="unpublished">unpublished</option>
+               </select>
+            </div>
+            <section className="max-w-[1200px] flex flex-wrap w-full gap-5 p-6 justify-center">
+               {filteredPosts.map((post) => {
+                  return (
+                     <PostCard
+                        key={post._id}
+                        post={post}
+                        deletePost={actions.deleteData}
+                        addPost={actions.addData}
+                     />
+                  );
+               })}
+               {data.length === 0 && <p>No posts</p>}
+            </section>
+         </div>
+      </div>
+   );
 }
