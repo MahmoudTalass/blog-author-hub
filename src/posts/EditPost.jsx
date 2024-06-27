3c6005b7426213d6bb594d32b35a47dbb6417311
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-06-27 19:39:53 -0400

    Create component for editing a post
    This component uses the PostForm component and has the logic to updating/
    editing a post.

---
+++ src/posts/EditPost.jsx
@@ -0,0 +1,52 @@
+import { usePostData } from "./usePostData";
+import { useNavigate, useParams } from "react-router-dom";
+import { PostForm } from "./PostForm";
+import { useFetch } from "../useFetch";
+
+export function EditPost() {
+   const { postId } = useParams();
+   const {
+      data: post,
+      error: fetchError,
+      isLoading: isFetching,
+   } = useFetch(`http://localhost:3000/api/posts/${postId}`);
+   const {
+      postData,
+      data: mutatedData,
+      error: mutationError,
+      isLoading: isMutating,
+   } = usePostData("http://localhost:3000/api/posts", "POST");
+   const navigate = useNavigate();
+
+   const submitForm = async (body) => {
+      if (body.isPublished && !post.post.isPublished) {
+         body["publishDate"] = Date.now();
+      }
+      body["_id"] = post.post._id;
+
+      await postData(body);
+      navigate(`/posts/${mutatedData._id}`);
+   };
+
+   if (isFetching) {
+      return <p>Loading...</p>;
+   }
+
+   if (fetchError) {
+      return <p>Something went wrong. Please try again later.</p>;
+   }
+
+   console.log(post);
+
+   return (
+      <section className="flex flex-col items-center mt-6">
+         <PostForm
+            error={mutationError}
+            submitForm={submitForm}
+            isLoading={isMutating}
+            post={post.post}
+            formType=""
+         />
+      </section>
+   );
+}
