4a2df38d954faef54d4a23f1deb5f75601ab8a46
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-06-27 10:04:59 -0400

    Create hook to delete a resource

---
+++ src/posts/useDeleteData.jsx
@@ -0,0 +1,47 @@
+import { useState } from "react";
+import { useAuthContext } from "../auth/useAuthContext";
+import { useLogout } from "../auth/useLogout";
+
+export function useDeleteData(url) {
+   const [error, setError] = useState(null);
+   const [isLoading, setIsLoading] = useState(false);
+   const { user } = useAuthContext();
+   const logout = useLogout();
+
+   const deleteData = async (body) => {
+      setIsLoading(true);
+      try {
+         const response = await fetch(url, {
+            method: "DELETE",
+            headers: {
+               Authorization: `Bearer ${user.token}`,
+               "content-type": "application/json",
+            },
+            body: JSON.stringify(body),
+         });
+
+         if (response.status === 401) {
+            logout();
+            throw new Error("Authorization required");
+         }
+
+         const json = await response.json();
+
+         if (response.status === 400) {
+            return setError(json.error);
+         }
+
+         if (!response.ok) {
+            throw new Error(json.error.message);
+         }
+
+         setError(null);
+      } catch (err) {
+         setError(err);
+      } finally {
+         setIsLoading(false);
+      }
+   };
+
+   return { deleteData, error, isLoading };
+}
