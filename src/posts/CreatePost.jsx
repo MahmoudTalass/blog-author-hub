4be1f516d89d9ec3d9595f540eae05ce9e767f74
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-06-29 17:09:26 -0400

    Use returned post instead of post state to get id
    Since by the time the fetch is done the state would not have updated,
    use the returned fetch item directly.

--- src/posts/CreatePost.jsx
+++ src/posts/CreatePost.jsx
@@ -3,18 +3,15 @@ import { useNavigate } from "react-router-dom";
 import { PostForm } from "./PostForm";
 
 export function CreatePost() {
-   const { postData, data, error, isLoading } = usePostData(
-      "http://localhost:3000/api/posts",
-      "POST"
-   );
+   const { postData, error, isLoading } = usePostData("http://localhost:3000/api/posts", "POST");
    const navigate = useNavigate();
 
    const submitForm = async (body) => {
       if (body.isPublished) {
          body["publishDate"] = Date.now();
       }
-      await postData(body);
-      navigate(`/posts/${data._id}`);
+      const newPost = await postData(body);
+      navigate(`/posts/${newPost._id}`);
    };
 
    return (
