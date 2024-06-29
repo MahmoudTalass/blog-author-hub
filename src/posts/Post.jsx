19251dc582dd774d997fb3e100b6ccc6669a16d9
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-06-29 17:08:36 -0400

    Use he and DOMPurify to get unescaped html and render it

--- src/posts/Post.jsx
+++ src/posts/Post.jsx
@@ -3,6 +3,8 @@ import { CommentSection } from "../comments/CommentsSection";
 import moment from "moment";
 import { useFetch } from "../useFetch";
 import { useAuthContext } from "../auth/useAuthContext";
+import DOMPurify from "dompurify";
+import { decode } from "he";
 
 export function Post() {
    const { user } = useAuthContext();
@@ -25,24 +27,20 @@ export function Post() {
       );
    }
 
-   function decodeHtml(html) {
-      var txt = document.createElement("textarea");
-      txt.innerHTML = html;
-      return txt.value;
-   }
-
-   const unescapedText = decodeHtml(post.text);
+   const decodedHtml = decode(post.text);
+   const sanitizedHtml = DOMPurify.sanitize(decodedHtml);
 
    return (
       <div className="w-full flex justify-center mt-8">
-         <section className="w-full sm:w-9/12 flex flex-col bg-[#1C2833] p-8 gap-12 rounded-xl ">
+         <section className="w-full sm:w-11/12 flex flex-col bg-[#1C2833] p-8 gap-12 rounded-xl ">
             <div className="flex flex-col gap-8">
                <h2 className="text-center sm:text-left text-3xl">{post.title}</h2>
                <div className="flex justify-between">
                   <p>{post.author.name}</p>
                   <p>{moment(post.publishDate).format("ll")}</p>
                </div>
-               <div className="text-lg" dangerouslySetInnerHTML={{ __html: unescapedText }}></div>
+               <hr />
+               <div className="p-4" dangerouslySetInnerHTML={{ __html: sanitizedHtml }}></div>
             </div>
             <CommentSection />
          </section>
