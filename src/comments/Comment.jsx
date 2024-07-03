4784799efe2406ed3b70ed70a84d97cb38da916c
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-07-03 18:11:56 -0400

    Call api using comment id and remove request body

--- src/comments/Comment.jsx
+++ src/comments/Comment.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from "react";
+import { useRef, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEllipsisVertical } from "@fortawesome/free-solid-svg-icons";
 import { useOutsideClick } from "../useOutsideClick";
@@ -6,11 +6,12 @@ import PropTypes from "prop-types";
 import moment from "moment";
 import { usePostData } from "../posts/usePostData";
 import { useDeleteData } from "../posts/useDeleteData";
+import { useAuthContext } from "../auth/useAuthContext";
 
 function Comment({ comment, commentActions, isCurrentUserComment }) {
    // toggle display state
    const [displayOptions, setDisplayOptions] = useState(false); // handles displaying the options tools for a comment (delete/edit)
-   const [isEditing, setIsEditing] = useState(false); // handles displying the input field for editing a comment
+   const [isEditing, setIsEditing] = useState(false); // handles displaying the input field for editing a comment
 
    // DOM ref
    const commentOptionsRef = useRef(null);
@@ -20,14 +21,15 @@ function Comment({ comment, commentActions, isCurrentUserComment }) {
 
    // state for text that the user will type to update/edit their comment
    const [commentInput, setCommentInput] = useState(comment.text);
-   const {
-      data: updatedComment,
-      error: updateError,
-      postData,
-   } = usePostData(`http://localhost:3000/api/comment/${comment._id}`);
-   const { deleteData, error: deleteError } = useDeleteData(
-      `http://localhost:3000/api/comment/${comment}`
+   const { error: updateError, postData } = usePostData(
+      `http://localhost:3000/api/comments/${comment._id}`,
+      "PUT"
    );
+   const { deleteData, error: deleteError } = useDeleteData(
+      `http://localhost:3000/api/comments/${comment._id}`
+   );
+
+   const { user } = useAuthContext();
 
    function handleDisplayEditing() {
       setCommentInput(comment.text);
@@ -37,11 +39,11 @@ function Comment({ comment, commentActions, isCurrentUserComment }) {
 
    const handleUpdateComment = async () => {
       const body = {
-         authorId: comment.author._id,
          text: commentInput,
       };
 
-      await postData(body);
+      const updatedComment = await postData(body);
+
       commentActions.updateData(updatedComment, updatedComment._id);
 
       if (!updateError) {
@@ -52,7 +54,7 @@ function Comment({ comment, commentActions, isCurrentUserComment }) {
    const handleDeleteComment = async () => {
       const commentId = comment._id;
       commentActions.deleteData(commentId);
-      await deleteData({ authorId: comment.author._id });
+      await deleteData();
 
       if (deleteError) {
          commentActions.addData(comment);
@@ -61,19 +63,19 @@ function Comment({ comment, commentActions, isCurrentUserComment }) {
 
    return (
       <>
-         <div className="flex gap-4 items-center bg-color3 p-2 rounded-lg">
+         <div className="flex gap-4 items-center bg-color3 p-2 rounded-lg ">
             {isEditing && (
                <div className="flex flex-col gap-2 flex-grow">
                   <input
                      type="text"
                      placeholder="comment..."
-                     className="w-full bg-inherit outline-none border-b"
+                     className="w-full bg-inherit border-b-4 border-solid"
                      value={commentInput}
                      onChange={(e) => setCommentInput(e.target.value)}
                   />
                   <div className="flex gap-2 justify-end">
                      <button
-                        className={"border rounded-full px-4 py-2 w-min text-sm"}
+                        className={"border rounded-full px-4 py-2 w-min text-sm bg-color1"}
                         title="cancel"
                         onClick={() => setIsEditing(false)}
                      >
@@ -83,23 +85,26 @@ function Comment({ comment, commentActions, isCurrentUserComment }) {
                         disabled={commentInput.length === 0}
                         className={`opacity-${
                            commentInput.length === 0 ? "10" : "100"
-                        } border rounded-full px-4 py-2 w-min text-sm`}
+                        } border rounded-full px-4 py-2 w-min text-sm bg-color1`}
                         title="save"
-                        onClick={() => handleSaveUpdate()}
+                        onClick={handleUpdateComment}
                      >
                         save
                      </button>
                   </div>
                   <div>
                      {updateError &&
-                        updateError.errors.length !== 0 &&
-                        updateError.errors.map((err) => {
-                           return (
-                              <p key={err.message} className="text-red-600">
-                                 {err.message}
-                              </p>
-                           );
-                        })}
+                        (updateError.errors?.length > 0 ? (
+                           updateError?.errors?.map((err) => {
+                              return (
+                                 <p key={err.message} className="text-red-500">
+                                    {err.message}
+                                 </p>
+                              );
+                           })
+                        ) : (
+                           <p className="text-red-500">{updateError.message}</p>
+                        ))}
                   </div>
                </div>
             )}
@@ -107,7 +112,12 @@ function Comment({ comment, commentActions, isCurrentUserComment }) {
                <>
                   <div className="flex-grow flex flex-col gap-1">
                      <div className="flex flex-col">
-                        <p className="font-bold">{comment.author.name}</p>
+                        <p className="font-bold">
+                           {comment.author.name}
+                           {comment.author._id === user.id && (
+                              <span className="italic"> (You)</span>
+                           )}{" "}
+                        </p>
                         <p className="text-sm text-gray-400">
                            {moment(comment.createdAt).fromNow()}
                         </p>
@@ -128,17 +138,13 @@ function Comment({ comment, commentActions, isCurrentUserComment }) {
                            role="menu"
                            ref={commentOptionsRef}
                         >
-                           {isCurrentUserComment}
                            {isCurrentUserComment && (
                               <button role="menuitem" onClick={handleDisplayEditing}>
                                  Edit
                               </button>
                            )}
                            <hr />
-                           <button
-                              role="menuitem"
-                              onClick={() => handleDeleteComment(comment._id, comment.author._id)}
-                           >
+                           <button role="menuitem" onClick={handleDeleteComment}>
                               Delete
                            </button>
                         </div>
@@ -155,7 +161,8 @@ function Comment({ comment, commentActions, isCurrentUserComment }) {
 
 Comment.propTypes = {
    comment: PropTypes.object,
-   commentActions: PropTypes.obj,
+   commentActions: PropTypes.object,
+   isCurrentUserComment: PropTypes.bool,
 };
 
 export default Comment;
