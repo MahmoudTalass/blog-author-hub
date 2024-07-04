3e8bd748c91d864543d20cc1278bee88aa102263
Mahmoud <86638781+MahmoudTalass@users.noreply.github.com>
2024-07-04 12:12:32 -0400

    When user login request is loading display loading indicator

--- src/auth/AuthButton.jsx
+++ src/auth/AuthButton.jsx
@@ -1,9 +1,11 @@
+import { faEllipsis } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import PropTypes from "prop-types";
 
 export function AuthButton({ children, disabled }) {
    return (
       <button className="border px-5 py-2 rounded-md" type="submit" disabled={disabled}>
-         {children}
+         {disabled ? <FontAwesomeIcon icon={faEllipsis} fade size="2x" /> : children}
       </button>
    );
 }
